48f09a19b467c197cfa194f3b1869405a4acbd46
Monczak <37554206+Monczak@users.noreply.github.com>
2020-02-27 15:55:38 +0100

    Touch control improvements

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -4,13 +4,45 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Input Properties")]
+    public float controlAreaSize;
     public float rotateSpeed;
+    public float singleTouchAccelTime;
+    public float accelFactor;
     public float dualTouchRotateSpeed;
+    public float dualToSingleSlowdown;
 
-    public float inertia;
+    [Header("Physics")]
+    public float momentOfInertia;
+    public float rotSpeedCap;
 
     private float rotInput;
     private float rotation;
+    private float singleTouchBeginTime;
+
+    private float oldDeltaY = 0;
+    private int oldTouchCount = 0;
+
+    private bool letGoOfDualSteering = false;
+
+    #region Touch Input Events
+
+    public delegate void OnDoubleTapDelegate();
+    public event OnDoubleTapDelegate OnDoubleTap;
+
+    public delegate void OnBeginMovementDelegate();
+    public event OnBeginMovementDelegate OnBeginMovement;
+
+    public delegate void OnEndMovementDelegate();
+    public event OnEndMovementDelegate OnEndMovement;
+
+    public delegate void OnBeginDualSteeringDelegate();
+    public event OnBeginDualSteeringDelegate OnBeginDualSteering;
+
+    public delegate void OnEndDualSteeringDelegate();
+    public event OnEndDualSteeringDelegate OnEndDualSteering;
+
+    #endregion
 
     // Start is called before the first frame update
     void Start()
@@ -21,17 +53,62 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        DetectGestures();
+        GetInput();
+
+        Move();
+    }
+
+    private void DetectGestures()
+    {
+
+    }
+
+    private void GetInput()
+    {
         int touchCount = Input.touchCount;
+
+        if (oldTouchCount == 2 && touchCount <= 1)
+        {
+            OnEndDualSteering?.Invoke();
+            letGoOfDualSteering = true;
+        }
 
         if (touchCount == 1)
         {
             Vector2 touchPos = Input.GetTouch(0).position;
 
-            if (touchPos.x < GameManager.Instance.screenWidth / 2) rotInput = rotateSpeed;
-            else rotInput = -rotateSpeed;
+            if (oldTouchCount == 0) singleTouchBeginTime = Time.time;
+
+            float preRotInput;
+
+            if (touchPos.x < GameManager.Instance.screenWidth * controlAreaSize / 2)
+                preRotInput = rotateSpeed;
+            else if (touchPos.x > GameManager.Instance.screenWidth - GameManager.Instance.screenWidth * controlAreaSize / 2)
+                preRotInput = -rotateSpeed;
+            else
+            {
+                touchCount = 0;
+                preRotInput = 0;
+            }
+
+            preRotInput *= (Time.time - singleTouchBeginTime) * singleTouchAccelTime + 1;
+
+            if (letGoOfDualSteering)
+                rotInput = Mathf.Lerp(rotInput, preRotInput, dualToSingleSlowdown * Time.deltaTime);
+            else
+                rotInput = preRotInput;
+
+            if (rotInput != 0 && oldTouchCount == 0) OnBeginMovement?.Invoke();
         }
         else if (touchCount == 2)
         {
+            if (oldTouchCount != 2)
+            {
+                if (oldTouchCount == 0) OnBeginMovement?.Invoke();
+                OnBeginDualSteering?.Invoke();
+            }
+
             Vector2[] touches = new Vector2[2];
             for (int i = 0; i < 2; i++)
                 touches[i] = Input.GetTouch(i).position;
@@ -43,33 +120,44 @@ public class PlayerController : MonoBehaviour
                 touches[1] = tmp;
             }
 
-            if (touches[0].x < GameManager.Instance.screenWidth / 2 && touches[1].x >= GameManager.Instance.screenWidth / 2)
+            if (touches[0].x < GameManager.Instance.screenWidth * controlAreaSize / 2 
+                && touches[1].x > GameManager.Instance.screenWidth - GameManager.Instance.screenWidth * controlAreaSize / 2)
             {
                 float deltaY = (touches[0].y - touches[1].y) / GameManager.Instance.screenHeight;
 
-                rotInput = -(deltaY * deltaY * dualTouchRotateSpeed) * Mathf.Sign(deltaY);
+                float acceleration = deltaY - oldDeltaY;
+
+                rotInput = -deltaY * deltaY * dualTouchRotateSpeed * Mathf.Sign(deltaY) - acceleration * accelFactor;
+
+                oldDeltaY = deltaY;
             }
             else
             {
-                if (touches[0].x < GameManager.Instance.screenWidth / 2) rotInput = rotateSpeed;
-                else rotInput = -rotateSpeed;
+                if (touches[0].x < GameManager.Instance.screenWidth * controlAreaSize / 2) rotInput = -rotateSpeed;
+                else if (touches[1].x > GameManager.Instance.screenWidth - GameManager.Instance.screenWidth * controlAreaSize / 2) rotInput = rotateSpeed;
+                else rotInput = 0;
             }
 
         }
         else
         {
             rotInput = 0;
+            letGoOfDualSteering = false;
+            OnEndMovement?.Invoke();
         }
 
-        Move();
+        Debug.Log(rotInput);
+        oldTouchCount = touchCount;
     }
 
-    void Move()
+    private void Move()
     {
         if (rotInput == 0)
-            rotation = Mathf.Lerp(rotation, 0, inertia * Time.deltaTime);
+            rotation = Mathf.Lerp(rotation, 0, momentOfInertia * Time.deltaTime);
         else 
-            rotation = Mathf.Lerp(rotation, rotInput, 5 * inertia * Time.deltaTime);
+            rotation = Mathf.Lerp(rotation, rotInput, 5 * momentOfInertia * Time.deltaTime);
+
+        rotation = Mathf.Clamp(rotation, -rotSpeedCap, rotSpeedCap);
 
         transform.Rotate(Vector3.forward * rotation * Time.deltaTime);
     }
