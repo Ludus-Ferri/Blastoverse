ddb87181c1503aca6cd54261c8f7c995c281db40
Monczak <37554206+Monczak@users.noreply.github.com>
2020-02-28 12:03:49 +0100

    Add score system

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -57,7 +57,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        OnDoubleTap += () => ScoreSystem.Instance.IncreaseScore(1000);
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/Managers/ScoreSystem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class ScoreSystem : MonoBehaviour
+{
+    public static ScoreSystem Instance;
+
+    public float score;
+
+    [Header("Score Growth Propeties")]
+    public int scorePerSecond;
+    public float scoreMultiplier;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        if (Instance != this) Destroy(gameObject);
+    }
+
+    public void IncreaseScore(int val)
+    {
+        score += Mathf.RoundToInt(val * scoreMultiplier);
+    }
+
+    public void DecreaseScore(int val)
+    {
+        score -= Mathf.RoundToInt(val * scoreMultiplier);
+        if (score < 0) score = 0;
+    }
+
+    private void Update()
+    {
+        score += scorePerSecond * Time.deltaTime;
+    }
+}
