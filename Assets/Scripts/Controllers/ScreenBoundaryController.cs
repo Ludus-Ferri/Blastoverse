a592a32e06c46db3957167c3c89f821109884e75
Monczak <37554206+Monczak@users.noreply.github.com>
2020-03-02 00:44:24 +0100

    Add TMPro, basic score text and a few fonts

--- Assets/Scripts/Controllers/ScreenBoundaryController.cs
+++ Assets/Scripts/Controllers/ScreenBoundaryController.cs
@@ -6,14 +6,14 @@ public class ScreenBoundaryController : MonoBehaviour
 {
     public float boundaryPadding;
 
-    BoxCollider2D collider;
+    BoxCollider2D col;
 
     private void Start()
     {
-        collider = GetComponent<BoxCollider2D>();
+        col = GetComponent<BoxCollider2D>();
 
         Vector2 screenSizeInUnits = GameManager.Instance.mainCamera.ScreenToWorldPoint(Vector2.one);
-        collider.size = new Vector2(Mathf.Abs(screenSizeInUnits.x * 2), Mathf.Abs(screenSizeInUnits.y * 2)) + Vector2.one * boundaryPadding;
+        col.size = new Vector2(Mathf.Abs(screenSizeInUnits.x * 2), Mathf.Abs(screenSizeInUnits.y * 2)) + Vector2.one * boundaryPadding;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
--- Assets/Scripts/Managers/ScoreSystem.cs
+++ Assets/Scripts/Managers/ScoreSystem.cs
@@ -4,17 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using TMPro;
 
 public class ScoreSystem : MonoBehaviour
 {
     public static ScoreSystem Instance;
 
-    float score;
-    public int Score
+    double score;
+    public long Score
     {
         get
         {
-            return Mathf.RoundToInt(score);
+            return (long)score;
         }
     }
 
@@ -22,25 +23,58 @@ public class ScoreSystem : MonoBehaviour
     public int scorePerSecond;
     public float scoreMultiplier;
 
+    [Header("UI Elements")]
+    public TMP_Text scoreText;
+    public Color zeroColor;
+    public int zeroPadding = 8;
+
     private void Awake()
     {
         if (Instance == null) Instance = this;
         if (Instance != this) Destroy(gameObject);
     }
 
-    public void IncreaseScore(int val)
+    public void IncreaseScore(long val)
     {
-        score += Mathf.RoundToInt(val * scoreMultiplier);
+        score += val * scoreMultiplier;
     }
 
-    public void DecreaseScore(int val)
+    public void DecreaseScore(long val)
     {
-        score -= Mathf.RoundToInt(val * scoreMultiplier);
+        score -= val * scoreMultiplier;
         if (score < 0) score = 0;
+    }
+
+    void UpdateScoreText()
+    {
+        StringBuilder builder = new StringBuilder();
+        builder.Append($"<color=#{ColorUtility.ToHtmlStringRGBA(zeroColor)}>");
+
+        if (Score.ToString().Length >= zeroPadding)
+        {
+            builder.Append("</color>");
+            builder.Append(Score.ToString($"D{zeroPadding}"));
+        }
+        else
+        {
+            builder.Append(Score.ToString($"D{zeroPadding}"));
+            for (int i = 18; i < builder.Length; i++)
+            {
+                if (builder[i] != '0' && builder[i - 1] == '0')
+                {
+                    builder.Insert(i, "</color>");
+                    break;
+                }
+            }
+        }
+
+        scoreText.text = builder.ToString();
     }
 
     private void Update()
     {
-        score += scorePerSecond * Time.deltaTime;
+        score += scorePerSecond * scoreMultiplier * Time.deltaTime;
+
+        UpdateScoreText();
     }
 }
