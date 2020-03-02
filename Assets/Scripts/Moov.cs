8b7bbc657fa2d421e821cf48665a490a5467ab22
KaMaTwo <tomst@vp.pl>
2020-03-02 13:08:10 +0100

    You all know that this code doesnt matter either way. We ll return here on some long friday night... But not now

---
+++ Assets/Scripts/Moov.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moov : MonoBehaviour
+{
+    double angle;
+    public int Bulletspeed = 10;
+    Rigidbody2D RB2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB2D = GetComponent<Rigidbody2D>();
+        RB2D.AddForce(new Vector2(Mathf.Cos(transform.rotation.eulerAngles.z), Mathf.Sin(transform.rotation.eulerAngles.z)) *Bulletspeed);
+        angle = transform.rotation.z; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,0,90*Time.deltaTime);
+    }
+}
