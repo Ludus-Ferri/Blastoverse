26438429805bf6597a1a89338940fb5487dd2bb0
Monczak <szymon.jedrzejczak.sj@gmail.com>
2020-03-05 22:08:09 +0100

    Add collision to asteroids

--- Assets/Scripts/Objects/Asteroid.cs
+++ Assets/Scripts/Objects/Asteroid.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Asteroid : MonoBehaviour
@@ -8,14 +9,24 @@ public class Asteroid : MonoBehaviour
     public int meshID;
 
     MeshFilter meshFilter;
+    PolygonCollider2D col;
 
     private void Awake()
     {
         meshFilter = GetComponent<MeshFilter>();
+        col = GetComponent<PolygonCollider2D>();
     }
 
     public void SetMesh()
     {
-        meshFilter.mesh = MeshStorage.Instance.GetAsteroidMeshFor(meshID);
+        Mesh mesh = MeshStorage.Instance.GetAsteroidMeshFor(meshID);
+
+        meshFilter.mesh = mesh;
+
+        Vector2[] colPath = new Vector2[mesh.vertexCount];
+        for (int i = 0; i < mesh.vertexCount; i++)
+            colPath[i] = new Vector2(mesh.vertices[i].x, mesh.vertices[i].y);
+
+        col.SetPath(0, colPath);
     }
 }
