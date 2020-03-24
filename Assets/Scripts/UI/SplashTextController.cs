6ce6bafba3ffafa07e82edfb603d1fd7745d1f00
Monczak <szymon.jedrzejczak.sj@gmail.com>
2020-03-25 00:48:19 +0100

    Add splash text controller

---
+++ Assets/Scripts/UI/SplashTextController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashTextController : MonoBehaviour
+{
+    public string splashStringPrefix;
+    public int splashCount;
+
+    private void Awake()
+    {
+        LocalizedText textComponent = GetComponent<LocalizedText>();
+
+        string text = splashStringPrefix + $".{Mathf.RoundToInt(Random.Range(1, splashCount))}";
+
+        textComponent.SetText(text);
+    }
+}
