fc739170dd9a2099e976572283839f0323f2aa59
Monczak <szymon.jedrzejczak.sj@gmail.com>
2020-03-25 00:35:47 +0100

    Add new menu button layout, better post processing

--- Assets/Scripts/Miscellaneous/LocalizedText.cs
+++ Assets/Scripts/Miscellaneous/LocalizedText.cs
@@ -6,9 +6,16 @@ using TMPro;
 public class LocalizedText : MonoBehaviour
 {
     public string textID;
+    public bool setOnAwake;
 
     private void Awake()
     {
+        if (setOnAwake) SetText(textID);
+    }
+
+    public void SetText(string id)
+    {
+        textID = id;
         GetComponent<TMP_Text>().text = LocalizedStringManager.GetLocalizedString(textID);
     }
 }
