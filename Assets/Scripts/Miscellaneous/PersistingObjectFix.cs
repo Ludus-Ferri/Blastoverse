f744d6e56ac87a86a2281913aa751d7b4df15459
Monczak <szymon.jedrzejczak.sj@gmail.com>
2020-03-26 14:43:31 +0100

    Fix #10

---
+++ Assets/Scripts/Miscellaneous/PersistingObjectFix.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistingObjectFix : MonoBehaviour
+{
+    void LateUpdate()
+    {
+        if (transform.childCount == 0)
+            Destroy(gameObject);
+    }
+}
