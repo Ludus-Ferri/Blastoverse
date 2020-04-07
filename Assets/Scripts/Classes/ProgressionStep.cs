3bd646d554126b1683f13a0f2bece56331c517f8
Monczak <szymon.jedrzejczak.sj@gmail.com>
2020-04-08 00:39:27 +0200

    Add progression system

---
+++ Assets/Scripts/Classes/ProgressionStep.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[Serializable]
+public struct ProgressionStep
+{
+    public long scoreThreshold;
+
+    [Header("Multipliers")]
+    public float scorePerSecondMult;
+    public float asteroidSpawnRateMult;
+    public float asteroidVelocityMult;
+    public float asteroidSizeMult;
+
+    [Header("Other")]
+    public Color backgroundColor;
+    public float musicIntensity;
+}
---
+++ Assets/Scripts/Managers/ProgressionSystem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressionSystem : MonoBehaviour
+{
+    public static ProgressionSystem Instance;
+
+    public List<ProgressionStep> progressionSteps;
+
+    ProgressionStep currentStep;
+
+    private void Awake()
+    {
+        #region Singleton
+        if (Instance == null) Instance = this;
+        if (Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        #endregion
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (ProgressionStep step in progressionSteps)
+        {
+            if (ScoreSystem.Instance.Score > step.scoreThreshold)
+            {
+                currentStep = step;
+                break;
+            }
+        }
+    }
+}
